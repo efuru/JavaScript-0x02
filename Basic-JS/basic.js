8aaf9ffbb1f6ae78d21ada64fa7de5b13edb6391
efuruanthonytech@yahoo.com <117752260+efuru@users.noreply.github.com>
2023-07-30 01:18:24 -0700

    just starting this

---
+++ Basic-JS/basic.js
@@ -0,0 +1,46 @@
+// inline comments
+
+/* this is 
+  a 
+  multiple
+  line
+  comments*/
+
+  var myName;
+
+  //Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+//Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+//initializing variables with assignment operators
+var a = 9;
+
+//Declare string variables
+var myFirstName = "Efuru";
+var myLastName = "Anthony";
+
+//uninitialized variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+//
+a = a + 1; //will give us 6
+b = b + 5;//will give us 15
+c = c + " String!"; //should give us "I am a String!"
+
+//Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
